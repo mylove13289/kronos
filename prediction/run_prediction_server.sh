3abfb5297aa78a004fb97161b9ddb2612fb2f68b
longquan <longquan@dian.so>
2025-09-02 15:37:17 +0800

    commit

--- prediction/run_prediction_server.sh
+++ prediction/run_prediction_server.sh
@@ -1,3 +1,3 @@
 
 
-/usr/local/bin/python3.10 /home/admin/software/kronos/github/kronos/update_predictions.py  --env server   --symbol BTCUSDT --interval 15m
+/usr/local/bin/python3.10 /home/admin/software/kronos/github/kronos/prediction/update_predictions.py  --env server   --symbol BTCUSDT --interval 15m
